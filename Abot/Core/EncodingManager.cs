260c9284a9762c978776a69c7e38d46430ea52b1
sjdirect <sjdirect@gmail.com>
2013-09-04 20:47:20 -0700

    112: Added to auto detect/handle response encodings, no tests

---
+++ Abot/Core/EncodingManager.cs
@@ -0,0 +1,125 @@
+﻿
+using Abot.Poco;
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+namespace Abot.Core
+{
+    public interface IEncodingManager
+    {
+        EncodedData GetEncodingData(WebResponse response);
+    }
+
+    public class EncodingManager : IEncodingManager
+    {
+        public EncodedData GetEncodingData(WebResponse response)
+        {
+            MemoryStream rawdata = GetRawData(response);
+
+            String charset = GetCharsetFromHeaders(response);
+
+            if (charset == null)
+                charset = GetCharsetFromBody(rawdata);
+
+
+            rawdata.Seek(0, SeekOrigin.Begin);
+
+            Encoding e = GetEncoding(charset);
+            string content = "";
+            using (StreamReader sr = new StreamReader(rawdata, e))
+            {
+                content = sr.ReadToEnd();
+            }
+
+            EncodedData eData = new EncodedData();
+            eData.CharsetString = charset;
+            eData.Content = content;
+            eData.Data = e.GetBytes(eData.Content);
+            eData.PageSizeInBytes = eData.Data.Length;
+            eData.Encoding = e;
+
+            return eData;
+        }
+
+        private string GetCharsetFromHeaders(WebResponse webResponse)
+        {
+            string charset = null;
+            String ctype = webResponse.Headers["content-type"];
+            if (ctype != null)
+            {
+                int ind = ctype.IndexOf("charset=");
+                if (ind != -1)
+                    charset = ctype.Substring(ind + 8);
+            }
+            return charset;
+        }
+
+        private string GetCharsetFromBody(MemoryStream rawdata)
+        {
+            String charset = null;
+
+            MemoryStream ms = rawdata;
+            ms.Seek(0, SeekOrigin.Begin);
+
+            String meta = "";
+            using (StreamReader srr = new StreamReader(ms, Encoding.ASCII))
+            {
+                meta = srr.ReadToEnd();
+            }
+
+            if (meta != null)
+            {
+                int start_ind = meta.IndexOf("charset=");
+                int end_ind = -1;
+                if (start_ind != -1)
+                {
+                    end_ind = meta.IndexOf("\"", start_ind);
+                    if (end_ind != -1)
+                    {
+                        int start = start_ind + 8;
+                        charset = meta.Substring(start, end_ind - start + 1);
+                        charset = charset.TrimEnd(new Char[] { '>', '"' });
+                    }
+                }
+            }
+
+            return charset;
+        }
+
+        private Encoding GetEncoding(string charset)
+        {
+            Encoding e = Encoding.UTF8;
+            if (charset != null)
+            {
+                try
+                {
+                    e = Encoding.GetEncoding(charset);
+                }
+                catch { }
+            }
+
+            return e;
+        }
+
+        private MemoryStream GetRawData(WebResponse webResponse)
+        {
+            MemoryStream rawdata = new MemoryStream();
+
+            using (Stream rs = webResponse.GetResponseStream())
+            {
+                byte[] buffer = new byte[1024];
+                int read = rs.Read(buffer, 0, buffer.Length);
+                while (read > 0)
+                {
+                    rawdata.Write(buffer, 0, read);
+                    read = rs.Read(buffer, 0, buffer.Length);
+                }
+            }
+
+            return rawdata;
+        }
+
+    }
+
+}
---
+++ Abot/Poco/EncodedData.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace Abot.Poco
+{
+    public class EncodedData
+    {
+        /// <summary>
+        /// String representation of the charset/encoding
+        /// </summary>
+        public string CharsetString { get; set; }
+
+        /// <summary>
+        /// The encoding of the web response
+        /// </summary>
+        public Encoding Encoding { get; set; }
+
+        /// <summary>
+        /// The raw content of the web response
+        /// </summary>
+        public string Content { get; set; }
+
+        /// <summary>
+        /// The raw data of the web response
+        /// </summary>
+        public byte[] Data { get; set; }
+
+        /// <summary>
+        /// The actual byte size of the web response's content. This property is due to the Content-length header being untrustable.
+        /// </summary>
+        public long PageSizeInBytes { get; set; }
+    }
+}
