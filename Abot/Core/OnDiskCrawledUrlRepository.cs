615ed2a0b3f0f9210a2cd7da34c79473cbe8cfca
sjdirect <sjdirect@gmail.com>
2014-01-19 20:52:21 -0700

    Added integration test for deep crawling, fixed port in dir name bug and added some logging.

--- Abot/Core/OnDiskCrawledUrlRepository.cs
+++ Abot/Core/OnDiskCrawledUrlRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using log4net;
+using System;
 using System.Collections.Concurrent;
 using System.Diagnostics;
 using System.IO;
@@ -11,6 +12,7 @@ namespace Abot.Core
 {
     public class OnDiskCrawledUrlRepository : ICrawledUrlRepository
     {
+        static ILog _logger = LogManager.GetLogger(typeof(OnDiskCrawledUrlRepository).FullName);
         IHashGenerator _hashGenerator;
         BlockingCollection<Uri> _urisToWriteToDisk = new BlockingCollection<Uri>();
         CancellationTokenSource _cancellationToken;
@@ -18,6 +20,16 @@ namespace Abot.Core
         object _directoryLocker = new object();
         object _hashLocker = new object();
         bool _deleteUriDbOnDispose;
+
+        /// <summary>
+        /// Uses file system as a db of crawled uris. Creates directories to represent a url crawled. This prevents OutOfMemoryException
+        /// by storing crawled uris to disk instead of holding them all in memory. This class should only be used when you expect a crawler
+        /// to encounter hundreds of thousands of links during a single crawl. Otherwise use the InMemoryCrawledUrlRepository
+        /// </summary>
+        public OnDiskCrawledUrlRepository()
+            :this(null)
+        {
+        }
 
         /// <summary>
         /// Uses file system as a db of crawled uris. Creates directories to represent a url crawled. This prevents OutOfMemoryException
@@ -36,9 +48,16 @@ namespace Abot.Core
             else
                 _uriDbDirectory = uriDbDirectory;
 
-            if (!_uriDbDirectory.Exists)
-                _uriDbDirectory.Create();
-
+            if (_uriDbDirectory.Exists)
+            {
+                _logger.WarnFormat("The directory [{0}] already exists and will be reused as the disk crawled url db. Any urls already stored there will not be recrawled", _uriDbDirectory.FullName);
+            }
+            else
+            {
+                _logger.InfoFormat("Creating directory [{0}] and will use as the on disk crawled url db", _uriDbDirectory.FullName);
+                _uriDbDirectory.Create();                
+            }
+                
             _deleteUriDbOnDispose = deleteUriDbDirectoryOnDispose;
 
             _cancellationToken = new CancellationTokenSource();
@@ -65,7 +84,10 @@ namespace Abot.Core
             _urisToWriteToDisk = new BlockingCollection<Uri>();
 
             if (_deleteUriDbOnDispose && Directory.Exists(_uriDbDirectory.FullName))
+            {
+                _logger.InfoFormat("Deleting directory [{0}] that was used as the crawled url db", _uriDbDirectory.FullName);
                 DeleteDirectory();
+            }
         }
 
         protected bool DirectoryExists(string path)
@@ -99,9 +121,11 @@ namespace Abot.Core
                 try
                 {
                     Directory.CreateDirectory(directoryName);
+                    _logger.DebugFormat("Creating directory [{0}] for uri [{1}]", directoryName, uri);
                 }
-                catch
+                catch(Exception e)
                 {
+                    _logger.WarnFormat("Error Creating directory [{0}] for uri [{1}]: {2}", directoryName, uri, e);
                 }
             }
             return true;
@@ -114,7 +138,7 @@ namespace Abot.Core
             {
                 hashedDirectoryName = BitConverter.ToString(_hashGenerator.GenerateHash(ASCIIEncoding.ASCII.GetBytes(uri.AbsoluteUri)));
             }
-            return Path.Combine(_uriDbDirectory.FullName, uri.Authority, hashedDirectoryName.Substring(0, 4), hashedDirectoryName);
+            return Path.Combine(_uriDbDirectory.FullName, uri.Host, hashedDirectoryName.Substring(0, 4), hashedDirectoryName);
         }
 
         protected void DeleteDirectory()
