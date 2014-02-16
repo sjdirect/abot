86dd86a28461f40ca5f07896f97fac3c9b66e444
sjdirect <sjdirect@gmail.com>
2014-02-15 17:33:46 -0700

    Fixes #8 AggregateException when cancellation toke is used to shutdown the crawl.

--- Abot/Core/TaskThreadManager.cs
+++ Abot/Core/TaskThreadManager.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 
 namespace Abot.Core
 {
+
     /// <summary>
     /// A ThreadManager implementation that will use tpl Tasks to handle concurrency.
     /// </summary>
@@ -37,7 +38,28 @@ namespace Abot.Core
 
         protected override void RunActionOnDedicatedThread(Action action)
         {
-            Task.Factory.StartNew(() => RunAction(action), _cancellationTokenSource.Token);
+            Task.Factory
+                .StartNew(() => RunAction(action), _cancellationTokenSource.Token)
+                .ContinueWith(HandleAggregateExceptions, TaskContinuationOptions.OnlyOnFaulted);
+        }
+
+        /// <summary>
+        /// This was added to resolve the issue described here
+        /// http://stackoverflow.com/questions/7883052/a-tasks-exceptions-were-not-observed-either-by-waiting-on-the-task-or-accessi
+        /// </summary>
+        private void HandleAggregateExceptions(Task task)
+        {
+            if (task == null || task.Exception == null) 
+                return;
+
+            var aggException = task.Exception.Flatten();
+            foreach (var exception in aggException.InnerExceptions)
+            {
+                if(_cancellationTokenSource.IsCancellationRequested)
+                    _logger.Warn(exception);//If the task was cancelled then this exception is expected happen and we dont care
+                else
+                    _logger.Error(exception);//If the task was not cancelled then this is an error
+            }
         }
     }
 }
