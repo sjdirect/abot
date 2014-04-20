07ddbf56d680608f26b587ff24773bc5d5c138e4
sjdirect <sjdirect@gmail.com>
2014-04-20 15:32:36 -0700

    Updated test

--- Abot.Tests.Integration/CrawlSiteSimulator.cs
+++ Abot.Tests.Integration/CrawlSiteSimulator.cs
@@ -31,7 +31,6 @@ namespace Abot.Tests.Integration
             
             CrawlConfiguration configuration = new CrawlConfiguration();
             configuration.MaxPagesToCrawl = 5;
-            configuration.MaxConcurrentThreads = 1;//TODO REMOVE ME, THIS WAS JUST FOR TESTING
 
             int pagesCrawledCount = 0;
 
