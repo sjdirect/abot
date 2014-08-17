c8d418c7d96d54db43ecebb87c96fa4a3905044f
sjdirect <sjdirect@gmail.com>
2014-08-17 15:24:51 -0700

    Changing assembly versions to 1.5.0.0

--- Abot.SiteSimulator/Properties/AssemblyInfo.cs
+++ Abot.SiteSimulator/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.2.3.1")]
-[assembly: AssemblyFileVersion("1.2.3.1")]
+[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
