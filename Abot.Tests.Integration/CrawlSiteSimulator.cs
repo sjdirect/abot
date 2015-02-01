276cffd0d352e3b76f06fb620c4d409d12421bee
sjdirect <sjdirect@gmail.com>
2015-01-31 20:15:28 -0700

    manual revert of last checkin

--- Abot.Tests.Integration/CrawlSiteSimulator.cs
+++ Abot.Tests.Integration/CrawlSiteSimulator.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace Abot.Tests.Integration
 {
-    [TestFixture, Ignore]
+    [TestFixture]
     public class CrawlSiteSimulator : CrawlTestBase
     {
         public CrawlSiteSimulator()
