c4c44c5d1e907ba86c827b31657eae7f6a69358a
sjdirect <sjdirect@gmail.com>
2015-02-01 21:46:00 -0700

    copied the assemblysetup in case that is causing the problem

--- Abot.Tests.Integration/AssemblySetup.cs
+++ Abot.Tests.Integration/AssemblySetup.cs
@@ -1,11 +1,27 @@
-﻿using Abot.Tests.Unit;
+﻿using System;
+using Abot.Tests.Unit;
 using log4net.Config;
 using NUnit.Framework;
 
 namespace Abot.Tests.Integration
 {
     [SetUpFixture]
-    public class AssemblySetup : Abot.Tests.Unit.AssemblySetup
+    public class AssemblySetup //: Abot.Tests.Unit.AssemblySetup
     {
+        [SetUp]
+        public void Setup()
+        {
+            XmlConfigurator.Configure();
+
+            FiddlerProxyUtil.StartAutoRespond(@"..\..\..\TestResponses.saz");
+            Console.WriteLine("Started FiddlerCore to autorespond with pre recorded http responses.");
+        }
+
+        [TearDown]
+        public void After()
+        {
+            FiddlerProxyUtil.StopAutoResponding();
+            Console.WriteLine("Stopped FiddlerCore");
+        }
     }
 }
