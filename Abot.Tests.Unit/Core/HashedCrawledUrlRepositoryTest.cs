5f5f2afc1e2ef8706ac4fcb411f03f4c91784e1c
sjdirect <sjdirect@gmail.com>
2015-11-05 19:29:52 -0700

    Added HashedCrawledUrlRepository but its ot the default

---
+++ Abot.Tests.Unit/Core/HashedCrawledUrlRepositoryTest.cs
@@ -0,0 +1,16 @@
+﻿using Abot.Core;
+using Commoner.Core.Testing;
+using NUnit.Framework;
+
+namespace Abot.Tests.Unit.Core
+{
+    [TestFixture]
+    public class HashedCrawledUrlRepositoryTest : CrawledUrlRepositoryTest
+    {
+        public override ICrawledUrlRepository GetInstance()
+        {
+            return new HashedCrawledUrlRepository();
+        }
+
+    }
+}
---
+++ Abot/Core/HashedCrawledUrlRepository.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Abot.Core
+{
+    /// <summary>
+    /// Implementation that stores a numeric hash of the url instead of the url itself to use for lookups. This should save space when the crawled url list gets very long. 
+    /// </summary>
+    public class HashedCrawledUrlRepository : ICrawledUrlRepository
+    {
+        private ConcurrentDictionary<long, byte> m_UrlRepository = new ConcurrentDictionary<long, byte>();
+
+        /// <inheritDoc />
+        public bool Contains(Uri uri)
+        {
+            return m_UrlRepository.ContainsKey(ComputeNumericId(uri.AbsoluteUri));
+        }
+
+        /// <inheritDoc />
+        public bool AddIfNew(Uri uri)
+        {
+            return m_UrlRepository.TryAdd(ComputeNumericId(uri.AbsoluteUri), 0);
+        }
+
+        /// <inheritDoc />
+        public virtual void Dispose()
+        {
+            m_UrlRepository = null;
+        }
+
+        private long ComputeNumericId(string p_Uri)
+        {
+            byte[] md5 = ToMd5Bytes(p_Uri);
+
+            long numericId = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                numericId += (long)md5[i] << (i * 8);
+            }
+
+            return numericId;
+        }
+
+        private static byte[] ToMd5Bytes(string p_String)
+        {
+            using (MD5 md5 = MD5.Create())
+            {
+                return md5.ComputeHash(Encoding.Default.GetBytes(p_String));
+            }
+        }
+    }
+}
