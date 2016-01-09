8e6496003c09656fb38265d852092e1e78733e52
sjdirect <sjdirect@gmail.com>
2016-01-08 23:53:18 -0700

    Broken build, appveyor must use an old compiler since string interpolation did not work

--- Abot.Tests.Unit/Core/DomainRateLimiterTest.cs
+++ Abot.Tests.Unit/Core/DomainRateLimiterTest.cs
@@ -249,7 +249,7 @@ namespace Abot.Tests.Unit.Core
             domainRateLimiter.RateLimit(domain);
             timer.Stop();
 
-            Assert.IsTrue(timer.ElapsedMilliseconds >= 150, $"Expected it to take more than 150 millisecs but only took {timer.ElapsedMilliseconds}");
+            Assert.IsTrue(timer.ElapsedMilliseconds >= 150, string.Format("Expected it to take more than 150 millisecs but only took {0}", timer.ElapsedMilliseconds));
         }
 
 
