d65450f74ed357dd6930f76a02f3184b0ebdab0a
Steven <sjdirect@gmail.com>
2018-12-26 16:38:25 -0800

    Moving hyperlink parser stubs and pocos.
     No logging, not compiling. In preparation for adding nuget anglesharp dependency.

---
+++ Abot2/Core/HyperLinkParser.cs
@@ -0,0 +1,162 @@
+﻿using Abot2.Poco;
+//using log4net;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Abot2.Poco;
+
+namespace Abot2.Core
+{
+    /// <summary>
+    /// Handles parsing html out of the raw html
+    /// </summary>
+    public interface IHtmlParser
+    {
+        /// <summary>
+        /// Parses html to extract hyperlinks, converts each into an absolute url representation.
+        /// </summary>
+        IEnumerable<HyperLink> GetLinks(CrawledPage crawledPage);
+    }
+
+    [Serializable]
+    public abstract class HyperLinkParser : IHtmlParser
+    {
+        //protected ILog _logger = LogManager.GetLogger("AbotLogger");
+        protected CrawlConfiguration _config;
+        protected Func<string, string> _cleanURLFunc;
+
+        protected HyperLinkParser()
+            :this(new CrawlConfiguration(), null)
+        {
+
+        }
+
+        protected HyperLinkParser(CrawlConfiguration config, Func<string, string> cleanURLFunc)
+        {
+            _config = config;
+            _cleanURLFunc = cleanURLFunc;
+        }
+
+        /// <summary>
+        /// Parses html to extract hyperlinks, converts each into an absolute url
+        /// </summary>
+        public virtual IEnumerable<HyperLink> GetLinks(CrawledPage crawledPage)
+        {
+            CheckParams(crawledPage);
+
+            Stopwatch timer = Stopwatch.StartNew();
+
+            List<HyperLink> links = GetUris(crawledPage, GetHrefValues(crawledPage))
+                .Select(hrv => new HyperLink(){ HrefValue = hrv})
+                .ToList();
+            
+            timer.Stop();
+            //_logger.DebugFormat("{0} parsed links from [{1}] in [{2}] milliseconds", ParserType, crawledPage.Uri, timer.ElapsedMilliseconds);
+
+            return links;
+        }
+
+        #region Abstract
+
+        protected abstract string ParserType { get; }
+
+        protected abstract IEnumerable<string> GetHrefValues(CrawledPage crawledPage);
+
+        protected abstract string GetBaseHrefValue(CrawledPage crawledPage);
+
+        protected abstract string GetMetaRobotsValue(CrawledPage crawledPage);
+
+        #endregion
+
+        protected virtual void CheckParams(CrawledPage crawledPage)
+        {
+            if (crawledPage == null)
+                throw new ArgumentNullException("crawledPage");
+        }
+
+        protected virtual List<Uri> GetUris(CrawledPage crawledPage, IEnumerable<string> hrefValues)
+        {
+            List<Uri> uris = new List<Uri>();
+            if (hrefValues == null || hrefValues.Count() < 1)
+                return uris;
+
+            //Use the uri of the page that actually responded to the request instead of crawledPage.Uri (Issue 82).
+            //Using HttpWebRequest.Address instead of HttpWebResonse.ResponseUri since this is the best practice and mentioned on http://msdn.microsoft.com/en-us/library/system.net.httpwebresponse.responseuri.aspx
+            Uri uriToUse = crawledPage.HttpWebRequest.Address ?? crawledPage.Uri;
+
+            //If html base tag exists use it instead of page uri for relative links
+            string baseHref = GetBaseHrefValue(crawledPage);
+            if (!string.IsNullOrEmpty(baseHref))
+            {
+                if (baseHref.StartsWith("//"))
+                    baseHref = crawledPage.Uri.Scheme + ":" + baseHref;
+
+                try
+                {
+                    uriToUse = new Uri(baseHref);
+                }
+                catch { }
+            }
+
+            string href = "";
+            foreach (string hrefValue in hrefValues)
+            {
+                try
+                {
+                    // Remove the url fragment part of the url if needed.
+                    // This is the part after the # and is often not useful.
+                    href = _config.IsRespectUrlNamedAnchorOrHashbangEnabled
+                        ? hrefValue
+                        : hrefValue.Split('#')[0];
+                    Uri newUri = new Uri(uriToUse, href);
+
+                    if (_cleanURLFunc != null)
+                        newUri = new Uri(_cleanURLFunc(newUri.AbsoluteUri));
+
+                    if (!uris.Exists(u => u.AbsoluteUri == newUri.AbsoluteUri))
+                        uris.Add(newUri);
+                }
+                catch (Exception e)
+                {
+                    //_logger.DebugFormat("Could not parse link [{0}] on page [{1}]", hrefValue, crawledPage.Uri);
+                    //_logger.Debug(e);
+                }
+            }
+
+            return uris;
+        }
+
+        protected virtual bool HasRobotsNoFollow(CrawledPage crawledPage)
+        {
+            //X-Robots-Tag http header
+            if(_config.IsRespectHttpXRobotsTagHeaderNoFollowEnabled)
+            {
+                var xRobotsTagHeader = crawledPage.HttpWebResponse.Headers["X-Robots-Tag"];
+                if (xRobotsTagHeader != null && 
+                    (xRobotsTagHeader.ToLower().Contains("nofollow") ||
+                     xRobotsTagHeader.ToLower().Contains("none")))
+                {
+                    //_logger.InfoFormat("Http header X-Robots-Tag nofollow detected on uri [{0}], will not crawl links on this page.", crawledPage.Uri);
+                    return true;
+                }   
+            }
+
+            //Meta robots tag
+            if (_config.IsRespectMetaRobotsNoFollowEnabled)
+            {
+                string robotsMeta = GetMetaRobotsValue(crawledPage);
+                if (robotsMeta != null &&
+                    (robotsMeta.ToLower().Contains("nofollow") ||
+                     robotsMeta.ToLower().Contains("none")))
+                {
+                    //_logger.InfoFormat("Meta Robots nofollow tag detected on uri [{0}], will not crawl links on this page.", crawledPage.Uri);
+                    return true;
+                }                
+                
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Abot2/Poco/CrawledPage.cs
@@ -0,0 +1,146 @@
+﻿//using HtmlAgilityPack;
+//using log4net;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using AngleSharp.Dom.Html;
+using AngleSharp.Parser.Html;
+
+namespace Abot2.Poco
+{
+    [Serializable]
+    public class CrawledPage : PageToCrawl
+    {
+        //ILog _logger = LogManager.GetLogger("AbotLogger");
+        HtmlParser _angleSharpHtmlParser;
+
+        //Lazy<HtmlDocument> _htmlDocument;
+        Lazy<IHtmlDocument> _angleSharpHtmlDocument;
+
+        public CrawledPage(Uri uri)
+            : base(uri)
+        {
+            _htmlDocument = new Lazy<HtmlDocument>(InitializeHtmlAgilityPackDocument);
+            _angleSharpHtmlDocument = new Lazy<IHtmlDocument>(InitializeAngleSharpHtmlParser);
+
+            Content = new PageContent();
+        }
+
+        ///// <summary>
+        ///// Lazy loaded Html Agility Pack (http://htmlagilitypack.codeplex.com/) document that can be used to retrieve/modify html elements on the crawled page.
+        ///// </summary>
+        //public virtual HtmlDocument HtmlDocument { get { return _htmlDocument.Value; } }
+
+        /// <summary>
+        /// Lazy loaded AngleSharp IHtmlDocument (https://github.com/AngleSharp/AngleSharp) that can be used to retrieve/modify html elements on the crawled page.
+        /// </summary>
+        public virtual IHtmlDocument AngleSharpHtmlDocument { get { return _angleSharpHtmlDocument.Value; } }
+
+        /// <summary>
+        /// Web request sent to the server
+        /// </summary>
+        public HttpWebRequest HttpWebRequest { get; set; }
+
+        /// <summary>
+        /// Web response from the server. NOTE: The Close() method has been called before setting this property.
+        /// </summary>
+        public HttpWebResponseWrapper HttpWebResponse { get; set; }
+
+        /// <summary>
+        /// The web exception that occurred during the crawl
+        /// </summary>
+        public WebException WebException { get; set; }
+
+        public override string ToString()
+        {
+            if(HttpWebResponse == null)
+                return Uri.AbsoluteUri;
+            else
+                return string.Format("{0}[{1}]", Uri.AbsoluteUri, (int)HttpWebResponse.StatusCode);
+        }
+
+        /// <summary>
+        /// Links parsed from page. This value is set by the WebCrawler.SchedulePageLinks() method only If the "ShouldCrawlPageLinks" rules return true or if the IsForcedLinkParsingEnabled config value is set to true.
+        /// </summary>
+        public IEnumerable<Uri> ParsedLinks { get; set; }
+
+        /// <summary>
+        /// The content of page request
+        /// </summary>
+        public PageContent Content { get; set; }
+
+        /// <summary>
+        /// A datetime of when the http request started
+        /// </summary>
+        public DateTime RequestStarted { get; set; }
+
+        /// <summary>
+        /// A datetime of when the http request completed
+        /// </summary>
+        public DateTime RequestCompleted { get; set; }
+
+        /// <summary>
+        /// A datetime of when the page content download started, this may be null if downloading the content was disallowed by the CrawlDecisionMaker or the inline delegate ShouldDownloadPageContent
+        /// </summary>
+        public DateTime? DownloadContentStarted { get; set; }
+
+        /// <summary>
+        /// A datetime of when the page content download completed, this may be null if downloading the content was disallowed by the CrawlDecisionMaker or the inline delegate ShouldDownloadPageContent
+        /// </summary>
+        public DateTime? DownloadContentCompleted { get; set; }
+
+        /// <summary>
+        /// The page that this pagee was redirected to
+        /// </summary>
+        public PageToCrawl RedirectedTo { get; set; }
+
+        /// <summary>
+        /// Time it took from RequestStarted to RequestCompleted in milliseconds
+        /// </summary>
+        public double Elapsed {
+            get {
+                return (RequestCompleted - RequestStarted).TotalMilliseconds;
+            }
+        }
+
+
+        //private HtmlDocument InitializeHtmlAgilityPackDocument()
+        //{
+        //    HtmlDocument hapDoc = new HtmlDocument();
+        //    hapDoc.OptionMaxNestedChildNodes = 5000;//did not make this an externally configurable property since it is really an internal issue to hap
+        //    try
+        //    {
+        //        hapDoc.LoadHtml(Content.Text);
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        hapDoc.LoadHtml("");
+
+        //        _logger.ErrorFormat("Error occurred while loading HtmlAgilityPack object for Url [{0}]", Uri);
+        //        _logger.Error(e);
+        //    }
+        //    return hapDoc;
+        //}
+
+        private IHtmlDocument InitializeAngleSharpHtmlParser()
+        {
+            if(_angleSharpHtmlParser == null)
+                _angleSharpHtmlParser = new HtmlParser();
+
+            IHtmlDocument document;
+            try
+            {
+                document = _angleSharpHtmlParser.Parse(Content.Text);
+            }
+            catch (Exception e)
+            {
+                document = _angleSharpHtmlParser.Parse("");
+
+                //_logger.ErrorFormat("Error occurred while loading AngularSharp object for Url [{0}]", Uri);
+                //_logger.Error(e);
+            }
+
+            return document;
+        }
+    }
+}
