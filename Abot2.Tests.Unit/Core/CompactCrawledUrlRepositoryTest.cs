85709e1f4b7adee4f0cecf633409ef0719789476
Steven <sjdirect@gmail.com>
2019-04-18 18:23:08 -0700

    Added CompactCrawledUrlRepository tests.

---
+++ Abot2.Tests.Unit/Core/CompactCrawledUrlRepositoryTest.cs
@@ -0,0 +1,15 @@
+﻿using Abot2.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Abot2.Tests.Unit.Core
+{
+    [TestClass]
+    public class CompactCrawledUrlRepositoryTest : CrawledUrlRepositoryTest
+    {
+        public override ICrawledUrlRepository GetInstance()
+        {
+            return new CompactCrawledUrlRepository();
+        }
+
+    }
+}
