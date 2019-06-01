13636b2043cbbe34a4a412811f2ef4865737ccc1
Steven <sjdirect@gmail.com>
2019-05-31 17:17:39 -0700

    Removed reliance on MD5 hashing to remove fips compliance issues. Fixes issue #156

--- Abot/Core/CompactCrawledUrlRepository.cs
+++ Abot/Core/CompactCrawledUrlRepository.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
 
 namespace Abot.Core
 {
@@ -12,45 +8,24 @@ namespace Abot.Core
     /// </summary>
     public class CompactCrawledUrlRepository : ICrawledUrlRepository
     {
-        private ConcurrentDictionary<long, byte> m_UrlRepository = new ConcurrentDictionary<long, byte>();
+        private ConcurrentDictionary<int, byte> m_UrlRepository = new ConcurrentDictionary<int, byte>();
 
         /// <inheritDoc />
         public bool Contains(Uri uri)
         {
-            return m_UrlRepository.ContainsKey(ComputeNumericId(uri.AbsoluteUri));
+            return m_UrlRepository.ContainsKey(uri.GetHashCode());
         }
 
         /// <inheritDoc />
         public bool AddIfNew(Uri uri)
         {
-            return m_UrlRepository.TryAdd(ComputeNumericId(uri.AbsoluteUri), 0);
+            return m_UrlRepository.TryAdd(uri.GetHashCode(), 0);
         }
 
         /// <inheritDoc />
         public virtual void Dispose()
         {
             m_UrlRepository = null;
-        }
-
-        protected long ComputeNumericId(string p_Uri)
-        {
-            byte[] md5 = ToMd5Bytes(p_Uri);
-
-            long numericId = 0;
-            for (int i = 0; i < 8; i++)
-            {
-                numericId += (long)md5[i] << (i * 8);
-            }
-
-            return numericId;
-        }
-
-        protected byte[] ToMd5Bytes(string p_String)
-        {
-            using (MD5 md5 = MD5.Create())
-            {
-                return md5.ComputeHash(Encoding.Default.GetBytes(p_String));
-            }
         }
     }
 }
